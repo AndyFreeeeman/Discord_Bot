2f02e007702e1d7adc65e0c1ba6d3622b22423d8
AndyFreeman <95344446+AndyFreeeeman@users.noreply.github.com>
2022-03-28 16:20:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import keep_alive
 
 #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
 client = commands.Bot(command_prefix="!",status=discord.Status.idle,activity=discord.Game(name="花兩小時建的環境"))
@@ -14,5 +13,4 @@ async def on_ready():
     
     
 # 下兩行順序不可交換
-keep_alive.keep_alive()   
 client.run(os.getenv('discord_token'))
