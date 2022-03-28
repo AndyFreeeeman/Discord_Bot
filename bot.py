ef7a2be9db57f9c3a62a9a2ce8f83ac50bd343cd
AndyFreeman <95344446+AndyFreeeeman@users.noreply.github.com>
2022-03-28 16:16:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+import keep_alive
 
+#discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+client = commands.Bot(command_prefix="!",status=discord.Status.idle,activity=discord.Game(name="花兩小時建的環境"))
+
+
+
+@client.event
+async def on_ready():
+    print(' >> discord bot: {0.user} is online. << '.format(client))
+    
+    
+# 下兩行順序不可交換
+keep_alive.keep_alive()   
+client.run(os.getenv('discord_token'))
